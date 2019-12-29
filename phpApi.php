a5af3d735635c6bd5f54798193332b62fc9723cf
vargabalazs900513@gmail.com <Flatron667>
2019-12-29 16:40:34 +0100

    műűködj!!

--- phpApi.php
+++ phpApi.php
@@ -181,26 +181,13 @@ function insertFamily($data){
     $fId = $data['fId'];
     $query="INSERT INTO Family SET   Name ='".$name."', FatherId=".$fId;
     
-    if(mysqli_query($connection, $query))   {
-        
-        $queryFamId = "SELECT Id FROM Family WHERE FatherId=".$fId." LIMIT 1";
-        //$result=mysqli_query($connection, $queryFamId);
-        //while($row=mysqli_fetch_array($result))  {
-  
-         // echo $querySetFatFam = "UPDATE User set FamilyId = ".$row."WHERE Id=".$fId;
-        
-          //}
-         // if(mysqli_query($connection, $querySetFatFam))   {
-                $response=array(
-                'status' => 1,
-                'status_message' =>'Family Added Successfully.'
+    if(mysqli_query($connection, $query))   {    
+    //    $queryFamId = "SELECT Id FROM Family WHERE FatherId=".$fId." LIMIT 1";
+        $response=array(
+           'status' => 1,
+           'status_message' =>'Family Added Successfully.'
                 );
-           // }else     {
-           //     $response=array(
-           //           'status' => 0,
-           //           'status_message' =>'Family Addition Failed.'
-           //           );
-           //  }
+         
       }
       else     {
          $response=array(
