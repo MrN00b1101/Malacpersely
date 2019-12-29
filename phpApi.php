85faf672e178e64546efa732e15c20e2fd80d1a8
vargabalazs900513@gmail.com <Flatron667>
2019-12-29 17:33:14 +0100

    mmm

--- phpApi.php
+++ phpApi.php
@@ -105,7 +105,8 @@ function insertCategory($data){
     $Name = $data['name'];
     $creaId = $data['creaId'];
     $global = $data['global'];
-    echo $query="INSERT INTO Categorys SET Name ='".$Name."', CreatorId='".$creaId."', Global='".$global."'";
+    echo $query="INSERT INTO Categorys SET Name ='".$Name."', CreatorId=".$creaId.", Global=".$global;
+    //echo $query="INSERT INTO Categorys SET Name ='teszt', CreatorId=9, Global=0;
     //echo $query="INSERT INTO 'User' VALUES ('dd', 'ff', 'gg')";
     if(mysqli_query($connection, $query))   {
          $response=array(
