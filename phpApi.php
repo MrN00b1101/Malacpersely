12d6a15d2a7d8bb39954dadffdf81045a20e9d26
vargabalazs900513@gmail.com <Flatron667>
2019-12-30 19:02:58 +0100

    kategória lekérés végleges

--- phpApi.php
+++ phpApi.php
@@ -383,7 +383,7 @@ function getCategoryList($userid, $fam){
         $response[]=$row;
     }
     header('Content-Type: application/json'); //header
-        //echo json_encode($response); //in JSON format }
-        echo json_encode($query); //in JSON format }
+        echo json_encode($response); //in JSON format }
+        //echo json_encode($query); //in JSON format }
 }
 ?>
