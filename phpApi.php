4388bdb59e36cb4f70ea1b12911bd4d7263d458d
vargabalazs900513@gmail.com <Flatron667>
2020-01-04 11:14:55 +0100

    login teszt

--- phpApi.php
+++ phpApi.php
@@ -24,6 +24,8 @@ switch($request_method) {
         case 'tran':
             if(isset($_SESSION['UserId'])){
                 getPersonTranList($_GET['user'],$_GET['cat'],$_GET['minVal'],$_GET['maxVal'],$_GET['minDat'],$_GET['maxDat'],$_GET['personal']);
+            }else{
+                header("HTTP/1.1 401 Need to login");
             }
         break;
         case 'cat':
