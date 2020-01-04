1df272f78e5b6645821503e380f076ad41ff4950
adamaclp92 <adamaclp92@gmail.com>
2020-01-04 17:53:19 +0100

    Bevétel_2

--- phpApi.php
+++ phpApi.php
@@ -542,6 +542,25 @@ function getPersonTranList($userId, $catId, $minVal, $maxVal, $minDat, $maxDat, 
     echo json_encode($response); 
     
 }
+
+function getTranList(){
+   
+    global $connection;
+  
+        $query = "SELECT * FROM Transactions";
+       
+        $response=array();
+        $query = $query.$szuro;
+        $result=mysqli_query($connection, $query);
+            while($row=mysqli_fetch_array($result))  {
+            $response[]=$row;
+        }
+    
+    header('Content-Type: application/json'); 
+    //echo json_encode($query); 
+    echo json_encode($response); 
+    
+}
 function getFamilyMemberList($famId, $http)
 {
     global $connection;
---
+++ vendor/bootstrap/js/tranzactions.js
@@ -0,0 +1,33 @@
+function getTranList(){
+    var request = new XMLHttpRequest();
+    var com = 'tran';
+    var user = 9;
+    var cat = 'null';
+    var minVal = 'null';
+    var maxVal = 'null';
+    var minDat = '2019-12-01';
+    var maxDat = 'null';
+    var personal = 1;
+    var token = getCookie("Token");
+    
+    var param = "?user="+user+"&cat="+cat+"&minVal="+minVal+"&maxVal="+maxVal+"&minDat='"+minDat+"'&maxDat="+maxDat+"&com="+com+"&personal="+personal+"&token="+token;
+
+    request.open('GET', 'phpApi.php',true)
+
+    request.onload = function() {
+    var obj = JSON.parse(request.response);
+    //alert(obj.length);
+    
+   
+    for(i=0; i< obj.length;i++){
+        if(obj[i].Value > 0)
+        document.getElementById("inComeValue") +=obj[i].Value;
+    }
+    //    alert(obj[2].Name);        
+        
+
+}
+// Send request
+request.send()
+//alert(getCookie("Token"));
+}
