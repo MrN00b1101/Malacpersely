ef3f210b7eb784a384ce73b79d8eb0685512b4b5
vargabalazs900513@gmail.com <Flatron667>
2020-01-04 11:46:31 +0100

    logout teszt

--- phpApi.php
+++ phpApi.php
@@ -571,7 +571,14 @@ function getUser($data, $type){
 }
 function logout()
 {
-    session_unset();
+    $_SESSION = array(); //destroy all of the session variables
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
     session_destroy();
 }
 function login($data){
