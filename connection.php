9fd9cedcd696048c9fca5b171dfdd1d0c728413d
vargabalazs900513@gmail.com <Flatron667>
2020-01-04 10:23:46 +0100

    connection string vissza állítás

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 Class dbObj{
-var $servername = "mrnoobrft.ddns.net"; 
-//var $servername = "localhost";
+//var $servername = "mrnoobrft.ddns.net"; 
+var $servername = "localhost";
 var $username = "neuman";
 var $password = "qwertz"; 
 var $dbname = "malacpersely"; 
