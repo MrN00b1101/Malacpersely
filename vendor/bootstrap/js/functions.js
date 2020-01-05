51ccf7e9d837ce6b9e6341f9b2600f187cc65707
adamaclp92 <adamaclp92@gmail.com>
2020-01-05 17:10:54 +0100

    kat

--- vendor/bootstrap/js/functions.js
+++ vendor/bootstrap/js/functions.js
@@ -98,7 +98,7 @@ function getTranList(){
         {
             document.getElementById("inComeId").innerHTML += inComeId+"<br>";
             inComeId++;
-            document.getElementById("inComeCategory").innerHTML += obj[i].Categorys.Name+"<br>";
+            document.getElementById("inComeCategory").innerHTML += obj[i].TranCatId+"<br>";
             document.getElementById("inComeValue").innerHTML += obj[i].Value+"<br>";
             document.getElementById("inComeDate").innerHTML += obj[i].TranDate+"<br>";
             document.getElementById("inOutSum").innerHTML += obj[i].TranDate+"<br>";
@@ -107,7 +107,7 @@ function getTranList(){
         else{
         document.getElementById("costId").innerHTML += costId+"<br>";
         costId++;
-        document.getElementById("costCategory").innerHTML += obj[i].Categorys.Name+"<br>";
+        document.getElementById("costCategory").innerHTML += obj[i].TranCatId+"<br>";
         document.getElementById("costValue").innerHTML += obj[i].Value+"<br>";
         document.getElementById("costDate").innerHTML += obj[i].TranDate+"<br>";
         inOutSum+=parseInt(obj[i].Value);
