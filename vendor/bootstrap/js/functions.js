56cdc81fe5229e3f6e43a2146d63b2ae0ebe916f
adamaclp92 <adamaclp92@gmail.com>
2020-01-08 20:25:02 +0100

    j

--- vendor/bootstrap/js/functions.js
+++ vendor/bootstrap/js/functions.js
@@ -123,10 +123,9 @@ function getTranList(){
             document.getElementById("inComeValue").innerHTML += obj[i].Value+"<br>";
             document.getElementById("inComeDate").innerHTML += obj[i].TranDate+"<br>";
             document.getElementById("inOutSum").innerHTML += obj[i].TranDate+"<br>";
-            inOutSum+=parseInt(obj[i].Value);
             document.getElementById("updateTrans").innerHTML += obj[i].TranDate+"<br>";
             document.getElementById("deleteTrans").innerHTML += '<button type="button" class="btn btn-secondary"> Törlés </button>'+"<br>";
-
+            inOutSum+=parseInt(obj[i].Value);
         }
         else{
         document.getElementById("costId").innerHTML += costId+"<br>";
@@ -134,8 +133,8 @@ function getTranList(){
         document.getElementById("costCategory").innerHTML += obj[i].TranCatId+"<br>";
         document.getElementById("costValue").innerHTML += obj[i].Value+"<br>";
         document.getElementById("costDate").innerHTML += obj[i].TranDate+"<br>";
-        inOutSum+=parseInt(obj[i].Value);
         document.getElementById("deleteTrans2").innerHTML += '<button type="button" class="btn btn-secondary"> Törlés </button>'+"<br>";
+        inOutSum+=parseInt(obj[i].Value);
         }
 }
 document.getElementById("inOutSum").innerHTML = inOutSum;
