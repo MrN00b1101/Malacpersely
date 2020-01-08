17e1943926d65dcc63d579230276daafff1f999d
adamaclp92 <adamaclp92@gmail.com>
2020-01-08 21:03:08 +0100

    s

--- vendor/bootstrap/js/functions.js
+++ vendor/bootstrap/js/functions.js
@@ -122,8 +122,8 @@ function getTranList(){
             document.getElementById("inComeCategory").innerHTML += obj[i].TranCatId+"<br>";
             document.getElementById("inComeValue").innerHTML += obj[i].Value+"<br>";
            document.getElementById("inComeDate").innerHTML += obj[i].TranDate+"<br>";
-           document.getElementById("updateTrans").innerHTML += '<option type="button" class="btn btn-secondary btn-sm"> Módosítás </option>'+"<br>";
-            document.getElementById("deleteTrans").innerHTML += '<option type="button" class="btn btn-secondary btn-sm"> Törlés </option>'+"<br>";
+           document.getElementById("updateTrans").innerHTML += '<option id="' + i + '" type="button" class="btn btn-secondary btn-sm"> Módosítás </option>'+"<br>";
+            document.getElementById("deleteTrans").innerHTML += '<option id="' + i + '" type="button" class="btn btn-secondary btn-sm"> Törlés </option>'+"<br>";
             inOutSum+=parseInt(obj[i].Value);
         }
         else{
@@ -132,8 +132,8 @@ function getTranList(){
         document.getElementById("costCategory").innerHTML += obj[i].TranCatId+"<br>";
         document.getElementById("costValue").innerHTML += obj[i].Value+"<br>";
         document.getElementById("costDate").innerHTML += obj[i].TranDate+"<br>";
-        document.getElementById("updateTrans2").innerHTML += '<option type="button" class="btn btn-secondary btn-sm"> Módosítás </option>'+"<br>";
-        document.getElementById("deleteTrans2").innerHTML += '<option type="button" class="btn btn-secondary btn-sm"> Törlés </option>'+"<br>";
+        document.getElementById("updateTrans2").innerHTML += '<option id="' + i + '" type="button" class="btn btn-secondary btn-sm"> Módosítás </option>'+"<br>";
+        document.getElementById("deleteTrans2").innerHTML += '<option id="' + i + '" type="button" class="btn btn-secondary btn-sm"> Törlés </option>'+"<br>";
         inOutSum+=parseInt(obj[i].Value);
         }
 }
@@ -269,7 +269,7 @@ function deleteCategory(){
     var com = 'cat';
     var table = 'Categorys';
     var el = document.getElementById('getCategories4');
-    alert(document.getElementById('getCategories4'));
+    //alert(document.getElementById('getCategories4'));
     var x = el.options[el.selectedIndex].value;
     var catArray = x.split(" - ");
     var id = parseInt(catArray[0]);
