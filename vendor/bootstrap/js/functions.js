7ca0cf275632d092eedc1d8d322ebe153e71cbef
adamaclp92 <adamaclp92@gmail.com>
2020-01-08 19:25:36 +0100

    s

--- vendor/bootstrap/js/functions.js
+++ vendor/bootstrap/js/functions.js
@@ -124,8 +124,8 @@ function getTranList(){
             document.getElementById("inComeDate").innerHTML += obj[i].TranDate+"<br>";
             document.getElementById("inOutSum").innerHTML += obj[i].TranDate+"<br>";
             inOutSum+=parseInt(obj[i].Value);
-            document.getElementById("updateTrans").innerHTML += '<option>' + 'Módosítás'+'</option>'+"<br>";
-            document.getElementById("deleteTrans").innerHTML += '<option>' + 'Törlés'+'</option>'+"<br>";
+            document.getElementById("updateTrans").innerHTML += '<button id="' + i + '">' + 'Módosítás'+'</option>'+"<br>";
+            document.getElementById("deleteTrans").innerHTML += '<button id="' + i + '">' + 'Törlés'+'</option>'+"<br>";
         }
         else{
         document.getElementById("costId").innerHTML += costId+"<br>";
@@ -134,8 +134,8 @@ function getTranList(){
         document.getElementById("costValue").innerHTML += obj[i].Value+"<br>";
         document.getElementById("costDate").innerHTML += obj[i].TranDate+"<br>";
         inOutSum+=parseInt(obj[i].Value);
-        document.getElementById("updateTrans2").innerHTML += '<option>' + 'Módosítás'+'</option>'+"<br>";
-        document.getElementById("deleteTrans").innerHTML += '<option>' + 'Törlés'+'</option>'+"<br>";
+        document.getElementById("updateTrans2").innerHTML += '<button id="' + i + '">' + 'Módosítás'+'</button>'+"<br>";
+        document.getElementById("deleteTrans").innerHTML += '<button id="' + i + '">' + 'Törlés'+'</option>'+"<br>";
         }
 }
 document.getElementById("inOutSum").innerHTML = inOutSum;
