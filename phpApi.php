706b3d837a796b5d7149611bea203d0c8336fc5f
vargabalazs900513@gmail.com <Flatron667>
2020-01-22 11:37:19 +0100

    mukodj

--- phpApi.php
+++ phpApi.php
@@ -570,7 +570,7 @@ function getCategoryList($userid, $fam){
     if($fam == '0'){
         $familyMembers = getFamilyMemberList(getFamilyId($userid),false);
         $uId = array_column($familyMembers,'Id');
-        $query = "SELECT User.Name as 'User',Categorys.* FROM Categorys INNER JOIN User ON Categorys.CreatorId = User.Id WHERE CreatorId = ".$userId;
+        $query = "SELECT User.Name as 'User',Categorys.* FROM Categorys INNER JOIN User ON Categorys.CreatorId = User.Id WHERE CreatorId = ".$userid;
 
        /* for ($i=1; $i<=count($uId)-1; $i++){
             $query = $query." OR CreatorId=".$uId[$i];
